2fe442a4c7afb7a2298dd2a27eb2e8a38ba750d4
Jack Howard <jack_howard@us.ibm.com>
2017-07-06 17:27:56 -0400

    improvements

--- import2iptools.py
+++ import2iptools.py
@@ -8,7 +8,7 @@ if len(sys.argv) != 2:
 	sys.exit("usage: api-snippets.py <csv input file>")
 f = open(sys.argv[1], 'rb')
 # how many lines are in the file (i.e. number of addresses to request)
-num_lines = sum(1 for line in f)
+#num_lines = sum(1 for line in f)
 f.seek(0)
 r = csv.reader(f)
 # Create an API client, replace token with your API token value
@@ -16,14 +16,15 @@ c = Client(token='your token here', host='lit-iptoolstest2.swg.usma.ibm.com')
 # Find the subnet you want to request addreses from, in this case I'll look for one named 'A3/188/Ratl' which has enough available addresses
 for row in r:
 	for subnet in c.ipv4subnets.all():
-		if subnet['name'] == (row[4]) and subnet['available'] >= num_lines:
-				print 'subnet %s\n' % (subnet['name'])
-				IPaddress = c.ipv4addresses.get(row[0])
-				user1 = c.users.get(row[3])
-				address = c.ipv4addresses.request(IPaddress['id'], assignee=[user1['id']])	
-				domain = c.domains.get(row[2])
-				name = c.domainnames.create(address['id'], row[1], domain['id'])
-				print 'Assigned %s.%s to %s\n' % (name['hostname'], name['domain']['name'], address['address'])
-				break
-		elif subnet['name'] == (row[4]) and subnet['available'] < num_lines:
-				print 'not enough room'	
+		if subnet['name'] == (row[4]):
+			print 'subnet %s has %s addresses left' % (subnet['name'], subnet['available'])
+			#if subnet['available'] >= num_lines:
+			IPaddress = c.ipv4addresses.get(row[0])
+			user1 = c.users.get(row[3])
+			address = c.ipv4addresses.request(IPaddress['id'], assignee=[user1['id']])	
+			domain = c.domains.get(row[2])
+			name = c.domainnames.create(address['id'], row[1], domain['id'])
+			print 'Assigned %s.%s to %s\n' % (name['hostname'], name['domain']['name'], address['address'])
+			break
+		#else:
+		#print 'not enough room'	
